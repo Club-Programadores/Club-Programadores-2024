8095de918c645ced51cd13bc2664b3047cec692d
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2023-10-21 14:24:58 +0000

    test

---
+++ src/generarBoxParticipante.js
@@ -0,0 +1,8 @@
+
+function boxParticipante(participante){
+    let divParticipante =  document.createElement("div");
+    let imgParticipante = document.createElement("img");
+    imgParticipante.src = participante.img;
+    
+    return divParticipante
+}
