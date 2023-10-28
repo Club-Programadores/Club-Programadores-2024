0be26b51bc1e94c8b1ae488d66ffb91c6dac83b2
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-10-28 19:00:04 +0000

    creado Hero banner

---
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -0,0 +1,16 @@
+import './HeroBanner.css'
+
+function HeroBanner(){
+    return (
+        <>
+        <div class="hero-banner">
+            <div className="call-to-action">
+                <h1 class="m-3">¡Sumate al Club de Programador@s del CUVL!</h1>
+                <button class="btn btn-outline-success m-3 bg-white">Sumate!</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default HeroBanner;
