67a0b49cff4edd842dac1d646393dacca33c7cff
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-10-29 22:43:35 +0000

    agregado placeholder para contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import HeroBanner from './components/HeroBanner/HeroBanner'
 import Navbar from './components/Navbar/Navbar'
 import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <HeroBanner />
       <About />
+      <Contact />
     </>
   )
 }
