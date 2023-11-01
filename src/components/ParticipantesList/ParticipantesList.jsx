6cd2642e17597df256b270dd2bae9c1194f024dc
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2023-11-01 00:14:57 +0000

    modifadd and implement participanteBox

--- src/components/ParticipantesList/ParticipantesList.jsx
+++ src/components/ParticipantesList/ParticipantesList.jsx
@@ -1,3 +1,4 @@
+import ParticipanteBox from '../ParticipanteBox/ParticipanteBox'
 import './ParticipantesList.css'
 
 export default function ParticipantesList(props){
@@ -6,23 +7,7 @@ export default function ParticipantesList(props){
         {
             props.participantes.map((participante)=>{
                 return (
-                    <div className='participanteBox' key={participante.id}>
-                        <div className='left'>
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                            <h1>{participante.nombre}</h1>
-                        </div>
-                        <div className='middle'></div>
-                        <div className='right'>
-                            <div className='iconsContainer'>
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                            </div>
-                        </div>
-                    </div>
+                    <ParticipanteBox data={participante}/>
                 )
             })
         }
