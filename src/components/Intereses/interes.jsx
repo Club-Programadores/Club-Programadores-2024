2941148961ddcc2154d251ae3394d81b5e7ad946
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2023-11-06 01:59:28 +0000

    branch participantes

---
+++ src/components/Intereses/interes.jsx
@@ -0,0 +1,11 @@
+import './interesStyles.css'
+
+export default function Interes(data){
+    return(
+        <div className='interesContainer'>
+            <h2>
+                {data.data}
+            </h2>
+        </div>
+    )
+}
--- src/components/ParticipanteBox/ParticipanteBox.jsx
+++ src/components/ParticipanteBox/ParticipanteBox.jsx
@@ -1,21 +1,31 @@
+import Interes from '..//Intereses//interes'
 
 export default function ParticipanteBox(participante){
     return (
         <div className='participanteBox' key={participante.data.id}>
-            <div className='left'>
+            <div className='profilePic' >
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                <h1>{participante.data.nombre}</h1>
             </div>
-            <div className='middle'></div>
-            <div className='right'>
-                <div className='iconsContainer'>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+            <div className='participanteInfo'>
+                <h1>{participante.data.nombre}</h1>
+                <hr/>
+                <div className='intereses'>
+                    <Interes data='GAMEDEV'></Interes>
+                    <Interes data='BACKEND'></Interes>
+                    <Interes data='BACKEND'></Interes>
+                    <Interes data='BACKEND'></Interes>
+                    <Interes data='BACKEND'></Interes>
+                    <Interes data='BACKEND'></Interes>
+                    <Interes data='BACKEND'></Interes>
                 </div>
+            </div>
+            <div className='iconsContainer'>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
             </div>
         </div>
     )
