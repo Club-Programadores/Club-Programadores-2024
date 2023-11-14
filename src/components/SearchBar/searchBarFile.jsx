77b3574413d5abd563c892e36197f27bc746cce2
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2023-11-14 23:39:36 +0000

    functional participantes searchBar

--- src/components/SearchBar/searchBarFile.jsx
+++ src/components/SearchBar/searchBarFile.jsx
@@ -5,9 +5,12 @@ import FilterButton from '../FilterButton/filterButtonFile'
 
 import './searchBarStyles.css'
 
-export default function SearchBar(){
-    const [name, setName] = useState('')
+export default function SearchBar(props){
     const searchIconRef = useRef(null);
+    
+    const handleOnChangeInput = e => {
+        props.setSearch(e.target.value)
+    }
 
     const show = e =>{
         if(searchIconRef.current.className != 'devicon-safari-line searchIcon  hidden')
@@ -26,8 +29,7 @@ export default function SearchBar(){
             <div className='container'>
                 <i className='devicon-safari-line searchIcon' ref={searchIconRef}></i>
                 <input className='searchInput'
-                    value={name}
-                    onChange={ e => setName(e.target.value)}
+                    onChange={handleOnChangeInput}
                     onFocus={show}
                     onBlur={hide}/>
             </div>
--- src/pages/Participantes/ParticipantesPage.jsx
+++ src/pages/Participantes/ParticipantesPage.jsx
@@ -1,23 +1,34 @@
+import { useState } from 'react'
+
 import Navbar from '../../components/Navbar/Navbar'
 import ParticipantesList from '../../components/ParticipantesList//ParticipantesList'
 import SearchBar from '../../components/SearchBar/searchBarFile.jsx'
+
 import './/ParticipantesStyles.css'
 
 function ParticipantesPage() {
+  const [search, setSearch] = useState('')
+
   const participantesArray = [
     {id:2, nombre:'Maite'},
     {id:3, nombre:'Veronica'},
     {id:4, nombre:'Mike'},
     {id:1, nombre:'Bautista'}
-]
+  ]
+
+  const filteredParticipantes = () =>{
+    if(search == ''){
+      return participantesArray; 
+    }
+    return participantesArray.filter(x => x.nombre.toLowerCase().startsWith(search.toLowerCase()))
+  }
 
   return (
     <>
       <Navbar/>
       <div className='listContainer'>
-        
-          <SearchBar/>
-        <ParticipantesList participantes={participantesArray}/>
+        <SearchBar setSearch={setSearch}/>
+        <ParticipantesList participantes={filteredParticipantes()}/>
       </div>
     </>
   )
