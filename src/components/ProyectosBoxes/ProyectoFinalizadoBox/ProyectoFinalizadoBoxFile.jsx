68a6d079f4f2912d3180849ec2fa698461acb3d4
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2023-11-15 02:25:49 +0000

    create branch proyectos + files

---
+++ src/components/ProyectosBoxes/ProyectoFinalizadoBox/ProyectoFinalizadoBoxFile.jsx
@@ -0,0 +1,11 @@
+
+import '../ProyectoBoxStyles.css'
+import './ProyectoFinalizadoBoxStyles.css'
+
+export default function ProyectoFinalizadoBox(){
+    return (
+        <div className="proyectoBox proyectoFinalizado">
+            <h1>Tittle</h1>
+        </div>
+    )
+}
