c9b47d81873fa1ee3ce208822dddb0c63f94d5eb
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2023-11-15 02:12:20 +0000

    add style to participanteBox & move styles the participanteBox got moved to its own css file

--- src/components/ParticipanteBox/ParticipanteBox.jsx
+++ src/components/ParticipanteBox/ParticipanteBox.jsx
@@ -1,5 +1,7 @@
 import Interes from '..//Intereses//interes'
 
+import './ParticipanteBoxStyles.css'
+
 export default function ParticipanteBox(participante){
     return (
         <div className='participanteBox' key={participante.data.id}>
@@ -8,8 +10,8 @@ export default function ParticipanteBox(participante){
             </div>
             <div className='participanteInfo'>
                 <h1>{participante.data.nombre}</h1>
-                <hr/>
                 <div className='intereses'>
+                    <h3>Intereses:</h3>
                     <Interes data='GAMEDEV'></Interes>
                     <Interes data='BACKEND'></Interes>
                     <Interes data='BACKEND'></Interes>
@@ -19,13 +21,16 @@ export default function ParticipanteBox(participante){
                     <Interes data='BACKEND'></Interes>
                 </div>
             </div>
-            <div className='iconsContainer'>
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+            <div>
+                <h3 style={{fontSize:'20px', margin:'0px'}}>SKILLS</h3>
+                <div className='iconsContainer'>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                </div>
             </div>
         </div>
     )
