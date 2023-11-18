099281fdb6c3f9192228f30b27a8821f13d1fa26
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-11-18 14:36:11 +0000

    first commit

--- src/components/buttons/SignUp/SignUp.jsx
+++ src/components/buttons/SignUp/SignUp.jsx
@@ -1,9 +1,21 @@
 import './SignUp.css'
+import SignUpModal from '../../SignUpModal/SignUpModal'
+
 
 function SignUp(){
+    function handleClick() {
+        alert("hiciste click en el boton de sumate");
+        
+
+      }
     return (
         <>
-            <button type="button" className="btn btn-outline-success" id="sumate">¡Sumate!</button>
+            <button type="button" 
+            className="btn btn-outline-success" 
+            id="sumate" 
+            onClick={handleClick}>
+                ¡Sumate!
+            </button>
         </>
     )
 }
