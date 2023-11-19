420eda32d1c9b3d058565e17830eaf0c43da8482
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2023-11-19 03:40:18 +0000

    implement json on proyectosPage + fixes

--- src/pages/Proyectos/ProyectosPage.jsx
+++ src/pages/Proyectos/ProyectosPage.jsx
@@ -5,23 +5,20 @@ import Footer from '../../components/Footer/Footer'
 import ProyectosList from '../../components/ProyectosList/proyectosListFile.jsx'
 import SearchBar from '../../components/SearchBar/searchBarFile.jsx'
 
+import ProyectosJson from '../../../assets/proyetos.json'
+
 import "./ProyectosStyles.css"
 
 function ProyectosPage() {
   const [search, setSearch] = useState('')
 
-  const proyectosArray = [
-    {id:1, titulo:'Proyecto 1', estado:'nuevo'},
-    {id:2, titulo:'Proyecto 2', estado:'finalizado'},
-    {id:3, titulo:'Proyecto 3', estado:'en_desarrollo'},
-    {id:4, titulo:'Proyecto 4', estado:'nuevo'}
-  ]
+  const proyectos = JSON.parse(JSON.stringify(ProyectosJson)).proyectos;
 
   const filteredProyectos = () =>{
     if(search == ''){
-      return proyectosArray; 
+      return proyectos; 
     }
-    return proyectosArray.filter(x => x.titulo.toLowerCase().startsWith(search.toLowerCase()))
+    return proyectos.filter(x => x.titulo.toLowerCase().startsWith(search.toLowerCase()))
   }
 
   return (
