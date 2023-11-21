adc1f01825afafc669246a3d0a2b3b902da9d61e
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2023-11-21 00:51:12 +0000

    rework proyects boxes

--- src/components/ProyectosBoxes/ProyectoFinalizadoBox/ProyectoFinalizadoBoxFile.jsx
+++ src/components/ProyectosBoxes/ProyectoFinalizadoBox/ProyectoFinalizadoBoxFile.jsx
@@ -5,7 +5,22 @@ import './ProyectoFinalizadoBoxStyles.css'
 export default function ProyectoFinalizadoBox(proyecto){
     return (
         <div className="proyectoBox proyectoFinalizado">
-            <h1>{proyecto.data.titulo}</h1>
+            <div className='info'>
+                <h1>{proyecto.data.titulo}</h1>
+                <p>{proyecto.data.descripcion}</p>
+            </div>
+            <div className='right'>
+                <a href={proyecto.data.url_pagina}>
+                    <button type="button" className="btn btn-primary">
+                        PAGINA
+                    </button>
+                </a>
+                <a href={proyecto.data.url_proyecto}>
+                    <button type="button" className="btn btn-dark">
+                        PROYECTO
+                    </button>
+                </a>
+            </div>
         </div>
     )
 }
