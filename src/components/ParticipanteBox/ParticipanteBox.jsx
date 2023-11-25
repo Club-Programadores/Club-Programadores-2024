688eef846cfbfaef9209f380af82d1c613e667a1
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2023-11-25 04:17:53 +0000

    responive participantes list

--- src/components/ParticipanteBox/ParticipanteBox.jsx
+++ src/components/ParticipanteBox/ParticipanteBox.jsx
@@ -15,24 +15,26 @@ export default function ParticipanteBox(participante){
     }
 
     return (
-        <div className='participanteBox'>
-            <div className='profilePic' >
-                <img src={participanteImageUrl()}/>
-            </div>
-            <div className='participanteInfo'>
-                <h1>{participante.data.nombre}</h1>
-                <div className='intereses'>
-                    <h3>Intereses:</h3>
-                    {
-                        participante.data.intereses.map((interes)=>{
-                            return(
-                                <Interes data={interes.toUpperCase()}></Interes>
-                            )
-                        })
-                    }
+        <div className='participanteBox container row'>
+            <div className='col-12 col-md-8' style={{display: 'flex'}}>
+                <div className='profilePic'>
+                    <img src={participanteImageUrl()}/>
+                </div>
+                <div className='participanteInfo'>
+                    <h1>{participante.data.nombre}</h1>
+                    <div className='intereses'>
+                        <h3>Intereses:</h3>
+                        {
+                            participante.data.intereses.map((interes)=>{
+                                return(
+                                    <Interes data={interes.toUpperCase()}></Interes>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
-            <div>
+            <div className='col-12 col-md-4' style={{paddingTop : '10px'}}>
                 <h3 style={{fontSize:'20px', margin:'0px'}}>SKILLS</h3>
                 <div className='iconsContainer'>
                     {
